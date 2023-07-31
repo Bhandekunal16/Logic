40b5b254607438ea5b6551349850afc709cf3c54
bhandekuanl16 <bhandekunal16@gmail.com>
2023-07-31 15:53:24 +0530

    update

--- condition.c
+++ condition.c
@@ -1,37 +1,7 @@
 #include<stdio.h>
+#include <stdlib.h>
 int main(){
-    int result = number();
-    if(result > 1 && result < 20){
-         printf("this is result %d\n", result );
-    }
-    else{
-         printf(" condition not meat %d\n", result );
-    }
-   return 0;
-}
-
-int number(){
-    int a, b, c, conform;
-    printf("if you want to guess a random number :") + scanf("%d", &conform);
-    if(conform==1){
-        printf("give me any number :") + scanf("%d", &a);
-        printf("give me another number :") + scanf("%d", &b);
-        c= a + b;
-        return c;
-    }
-    else{
-        printf("thank you !");
-        return 0;
-    }
-}
-
-int array(){
-    int num= number();
-    int Array[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20};
-    if(num >1 && num < 10){
-        return Array[num];
-    }
-    else{
-        return 0;
-    }
+    int* dynamicArray = (int*)malloc(1000 * sizeof(int));
+    printf("%d",dynamicArray);
+    return 0;
 }
