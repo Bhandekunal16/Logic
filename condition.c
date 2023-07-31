93663ce103378d567ee155ff11377e03cfee7362
bhandekuanl16 <bhandekunal16@gmail.com>
2023-07-31 13:43:54 +0530

    update

---
+++ condition.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int result = number();
+    if(result > 1 && result < 10){
+         printf("this is result %d\n", result );
+    }
+    else{
+         printf("condition not meat %d\n", result );
+    }
+   return 0;
+}
+
+int number(){
+    int a, b, c;
+    printf("give me any number") + scanf("%d", &a);
+    printf("give me another number") + scanf("%d", &b);
+    c= a + b;
+    return c;
+}
+
+int array(){
+    int num= number();
+    int Array[10]={1,2,3,4,5,6,7,8,9,10};
+    if(num >1 && num < 10){
+        return Array[num];
+    }
+    else{
+        return 0;
+    }
+}
