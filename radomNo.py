e2db52722524e59e5f4137931168c18bbbac04d7
bhandekuanl16 <bhandekunal16@gmail.com>
2023-07-31 16:59:34 +0530

    update

---
+++ radomNo.py
@@ -0,0 +1,12 @@
+import random
+
+a= int(input("give me number :"))
+b= int(input("give me your chooses number :"))
+
+if a == "1":  
+    random_number = random.randint(1, b)  
+    print("Random Number:", random_number)
+
+else : 
+    random_number = random.randint(1, 1000)  
+    print("Random Number:", random_number)
--- randomNo.c
+++ randomNo.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 #include <stdlib.h>
 int main(){
-    int* dynamicArray = (int*)malloc(1000 * sizeof(int));
-    printf("%d",dynamicArray);
-    return 0;
+    int a,b;
+    printf("if you want to add your num print 1 :") + scanf("%d", &a);
+    if(a==1){
+        printf("enter your number" ) + scanf("%d", &b);
+        int* dynamicArray = (int*)malloc( b* sizeof(int));
+        printf("%d",dynamicArray);
+    }
+    else {
+        int* dynamicArray2 = (int*)malloc(1000 * sizeof(int));
+        printf("%d",dynamicArray2);
+    }
+   return 0;
 }
