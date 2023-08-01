96f07748ec67b860397f3202f3a4ebcbfc48a511
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-01 11:42:24 +0530

    update

--- equation.c
+++ equation.c
@@ -9,16 +9,29 @@ int main(){
     printf("value of y : %f\n", y);
    printf("value of x : %f\n",x );
    int main2= sec();
+   int main3= three();
     return 0;
 }
 
 int sec(){
-    printf("2x - 4y = 4 = 10 this is equation ");
+    printf("2x - 4y = 10 this is equation ");
     float x ,y;
     printf("give me number for x :") + scanf("%f", &x);
     // 2x - 4y = 4
     y = (4 - 2 * x) / 4;
     x = (4 - 4 * y) / 2;
+   printf("value of y : %f\n", y);
+   printf("value of x : %f\n",x );
+    return 0;
+}
+
+int three(){
+     printf("2x + 3y = 8 this is equation ");
+    float x ,y;
+    printf("give me number for x :") + scanf("%f", &x);
+    // 2x - 4y = 4
+    y = (8 - 2 * x) / 3;
+    x = (8 - 3 * y) / 2;
    printf("value of y : %f\n", y);
    printf("value of x : %f\n",x );
     return 0;
--- equation.py
+++ equation.py
@@ -6,7 +6,13 @@ print("this is your y :", y)
 print("this is your x :", x)
 print("2x - 4y = 4 = 10 this is equation")
 x=float(input("give me number for :"))
-y = (4 - 2 * x) / 4;
-x = (4 - 4 * y) / 2;
+y = (4 - 2 * x) / 4
+x = (4 - 4 * y) / 2
+print("this is your y :", y)
+print("this is your x :", x)
+print("2x + 3y = 8 this is equation")
+x=float(input("give me number for :"))
+y = (8 - 2 * x) / 3
+x = (8 - 3 * y) / 2
 print("this is your y :", y)
 print("this is your x :", x)
