7db7d6b4c3524942e16193aa22a06904e49cea28
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-02 10:56:15 +0530

    circumference updated

---
+++ c/circumfarnce-couter.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    float r, pi= 3.14, c;
+    printf("give me radius of circle :") + scanf("%f", &r);
+    c= 2 * pi * r;
+    printf("this is circumference of circle  %f\n",c);
+    return 0;
+}
