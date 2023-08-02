4330839ea35b3363b9bf1962808953d33866a1d1
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-02 10:32:07 +0530

    add even-odd

---
+++ even-odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a; 
+    printf("give the number : ") + scanf("%d", &a);
+    if(a %2 ==0)
+    {
+        printf("number is even %d\n", a);
+    }
+    else {
+        printf("number is odd %d\n", a);
+    }
+    return 0;
+}
