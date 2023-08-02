2afadcacc69606cfaab11a8ab4a71222baad3dbb
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-02 11:15:45 +0530

    add percentage- counter

---
+++ c/percentage-counter.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float number,totalNumber,percentage;
+    printf("give the number :") + scanf("%f", &number);
+    printf("give the total number :") + scanf("%f", &totalNumber);
+    percentage = number / totalNumber * 100;
+    printf(" this is your percentage : %f\n", percentage);
+    return 0;
+}
