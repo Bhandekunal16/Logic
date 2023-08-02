1bf33cba4b0b62a66b9c483d4a462ec613efb093
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-02 15:35:01 +0530

    add ui

---
+++ home.js
@@ -0,0 +1,23 @@
+
+const homeContent = () => {
+  return ` <div style="background-color: rgb(216, 216, 216); padding: 10%; border-radius: 8px;">
+  <div style="text-align: center;">
+      <h1>contribute create a good code environment.</h1>
+  </div>
+  <div style="background-color: whitesmoke; border-radius: 8px;">
+      <li>
+          <a href="https://github.com/Bhandekunal16/Logic/tree/main/c">c </a>
+      </li>
+      <li>
+         <a href="https://github.com/Bhandekunal16/Logic/tree/main/javascript"> javascript</a>
+      </li>
+      <li>
+          <a href="https://github.com/Bhandekunal16/Logic/tree/main/python">python</a>
+      </li>
+  </div>
+</div>`;
+};
+
+function home(){
+    appContent.innerHTML= homeContent()
+  }
