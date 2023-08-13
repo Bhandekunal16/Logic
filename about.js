629f3a879c56fb1fd4bd9818bb20b3eb1bf3abb7
smart school <you@example.com>
2023-08-13 09:54:52 +0530

    update

--- about.js
+++ about.js
@@ -2,9 +2,8 @@ const aboutContent = () => {
     return `
       <h2>Our Company</h2>
       <p>This is the About page where you can provide information about your company, organization, or yourself.</p>
-      <div>
-      <a href="https://bhandekunal16.github.io/Resume/">about me</a>
-      </div>
+      <p><a href="https://bhandekunal16.github.io/Resume/">about me</a></p>
+      <p> <a href="https://bhandekunal16.github.io/Introduction/">it's my journey.</a></p>
       <p>Feel free to add details about your company's history, mission, vision, team members, or any other relevant information.</p>
     `;
   };
