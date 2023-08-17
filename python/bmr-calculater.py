a701cd00a2da8397bd1cfc232518fe0c3e4200a0
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-17 16:20:28 +0530

    bmr-calculater.py added

---
+++ python/bmr-calculater.py
@@ -0,0 +1,5 @@
+weight = float(input(" give me the weight :"))
+height = float(input(" give me the height :"))
+age = float(input("give me the age :"))
+result = 13.397 * weight + 4.799 * height - 5.677 * age + 88.362
+print(result)
