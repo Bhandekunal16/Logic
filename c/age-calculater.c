d106fd66a218b76dbc3fd4c6861823a644a8bf83
bhandekuanl16 <bhandekunal16@gmail.com>
2023-08-17 16:35:47 +0530

    add age-calculater.c

---
+++ c/age-calculater.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int birthYear;
+    time_t currentTime;
+    struct tm *timeInfo;
+
+    time(&currentTime);
+    timeInfo = localtime(&currentTime);
+    int currentYear = timeInfo->tm_year + 1900;
+
+    printf("Enter your birth year: ");
+    scanf("%d", &birthYear);
+
+    int age = currentYear - birthYear;
+    printf("Your age: %d\n", age);
+
+    return 0;
+}
